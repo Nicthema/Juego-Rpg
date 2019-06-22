a17a20933a2294d21deb99f88ca142bbd48266b5
Peu <48685783+Nicthema@users.noreply.github.com>
2019-06-22 10:45:11 -0300

    Update index.js

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,26 +3,43 @@ let items = document.getElementsByClassName('items');
 let shelf = document.getElementsByClassName('shelf');
 let itemSeleccionado;
 
-for(let item of items) {
-    item.onclick = function() {
+for (let item of items) {
+    item.onclick = function () {
         itemSeleccionado = item.innerHTML;
     }
 }
 
-for(let i = 0; i < shelf.length; i++) {
-    shelf[i].onclick = function() {
+for (let i = 0; i < shelf.length; i++) {
+    shelf[i].onclick = function () {
         itemsShelf[i].push(itemSeleccionado);
+        limitarItems();
         actualizarShelf();
+
     }
 }
 
-const actualizarShelf = function() {
-    for(let i = 0; i < shelf.length; i++) {
+const actualizarShelf = function () {
+    for (let i = 0; i < shelf.length; i++) {
         shelf[i].innerHTML = '';
-        for(let j = 0; j < itemsShelf[i].length; j++) {
+        for (let j = 0; j < itemsShelf[i].length; j++) {
             let nuevoItem = document.createElement('div');
             nuevoItem.innerHTML = itemsShelf[i][j];
             shelf[i].appendChild(nuevoItem);
-            }
+
         }
+
     }
+
+}
+
+const limitarItems = function() {
+    for (let i = 0; i < shelf.length; i++) {
+        for (let j = 0; j < itemsShelf[i].length; j++) {
+            if(itemsShelf[i].length >= 2) {
+                itemsShelf[i].length = 2;
+            }
+
+        }
+
+    }
+}
